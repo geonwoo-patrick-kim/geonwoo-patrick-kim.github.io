7611888952d93ef816d85e1d800e144687212077
Patrick Kim <122371333+geonwoo-patrick-kim@users.noreply.github.com>
2026-07-16 18:40:25 -0500

    Delete assets directory

--- assets/js/site.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  const image = document.querySelector('[data-portrait-image]');
-  if (!image) return;
-
-  const hidePortrait = () => document.body.classList.add('no-portrait');
-  if (image.complete && image.naturalWidth === 0) hidePortrait();
-  image.addEventListener('error', hidePortrait, { once: true });
-})();
